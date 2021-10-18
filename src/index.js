0860c45680a88d768e0c6962239999ca227aa190
kennyyuenhk01 <kennyyuen@hk01.com>
2021-10-18 15:16:41 +0800

    added remote table

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import {
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 import locale from 'antd/lib/locale/zh_CN';
+import reqwest from 'reqwest';
 
 const { Header, Sider, Content, Footer } = Layout;
 
@@ -65,9 +66,38 @@ const columns = [
   },
 ];
 
-const data = [];
+const columns2 = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: true,
+    render: name => `${name.first} ${name.last}`,
+    width: '20%',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    filters: [
+      { text: 'Male', value: 'male' },
+      { text: 'Female', value: 'female' },
+    ],
+    width: '20%',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+];
+
+const getRandomuserParams = params => ({
+  results: params.pagination.pageSize,
+  page: params.pagination.current,
+  ...params,
+});
+
+const data1 = [];
 for (let i = 0; i < 100; i++) {
-  data.push({
+  data1.push({
     key: i,
     ordering: i+1,
     campaignCodeNum: `TH00000${i}`,
@@ -82,9 +112,6 @@ function callback(key) {
 }
 
 class SiderDemo extends React.Component {
-  state = {
-    collapsed: false,
-  };
 
   toggle = () => {
     this.setState({
@@ -92,7 +119,56 @@ class SiderDemo extends React.Component {
     });
   };
 
+  state = {
+    collapsed: false,
+    data: [],
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+    loading: false,
+  };
+
+  componentDidMount() {
+    const { pagination } = this.state;
+    this.fetch({ pagination });
+  }
+
+  handleTableChange = (pagination, filters, sorter) => {
+    this.fetch({
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      pagination,
+      ...filters,
+    });
+  };
+
+  fetch = (params = {}) => {
+    this.setState({ loading: true });
+    reqwest({
+      url: 'https://randomuser.me/api',
+      method: 'get',
+      type: 'json',
+      data: getRandomuserParams(params),
+    }).then(data => {
+      console.log(data);
+      this.setState({
+        loading: false,
+        data: data.results,
+        pagination: {
+          ...params.pagination,
+          total: 200,
+          // 200 is mock data, you should read it from server
+          // total: data.totalCount,
+        },
+      });
+    });
+  };
+
   render() {
+
+    const { data, pagination, loading } = this.state;
+
     return (
       <Layout>
         <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
@@ -157,10 +233,17 @@ class SiderDemo extends React.Component {
                       </Button>,
                     ]}
                   ></PageHeader>
-                <Table columns={columns} dataSource={data} scroll={{ x: "auto", y: 600 }} style={{padding:24}}/>
+                <Table columns={columns} dataSource={data1} scroll={{ x: "auto", y: 600 }} style={{padding:24}}/>
               </TabPane>
               <TabPane tab="主頁專題管理" key="2">
-                Content of Tab Pane 2
+                <Table
+                  columns={columns2}
+                  rowKey={record => record.login.uuid}
+                  dataSource={data}
+                  pagination={pagination}
+                  loading={loading}
+                  onChange={this.handleTableChange}
+                />
               </TabPane>
             </Tabs>
 
